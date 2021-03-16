8ff55f49e2d4acf651b5caa5f8353dea23ce5907
Naveen Bisht <navinbisht7@gmail.com>
2021-03-16 22:03:37 +0530

    Updates

---
+++ precache-manifest.eccf9d9fdf9a9373e48af0cd8bc6f7d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfa03f72188e647510dfaf9d31a6f18",
+    "url": "/GiphySearch/index.html"
+  },
+  {
+    "revision": "a61854c2e28e430e8be1",
+    "url": "/GiphySearch/static/css/main.bad966da.chunk.css"
+  },
+  {
+    "revision": "ddf22b1f51de7c704d0c",
+    "url": "/GiphySearch/static/js/2.c0df61fd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/GiphySearch/static/js/2.c0df61fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a61854c2e28e430e8be1",
+    "url": "/GiphySearch/static/js/main.35957089.chunk.js"
+  },
+  {
+    "revision": "0db3df6034da2ac7f8c1",
+    "url": "/GiphySearch/static/js/runtime-main.11054673.js"
+  }
+]);
